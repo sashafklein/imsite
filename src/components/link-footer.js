9cfde4e04f2fe49fbe5967398af2b403ed1aa0e8
Sasha Klein <sashafklein@gmail.com>
2020-04-09 14:31:38 -0700

    Link footer height fix

--- src/components/link-footer.js
+++ src/components/link-footer.js
@@ -6,7 +6,7 @@ import Button from "./button"
 
 const LinkFooter = ({ to, text }) => (
   <div className="bg--royal-blue">
-    <div className="container--large h115 h100--resp flex flex-jc--fe flex-jc--c--resp flex-ai--c">
+    <div className="container--large h125 h100--resp flex flex-jc--fe flex-jc--c--resp flex-ai--c">
       <Button type="transparent,icon" to={to} className="color--white">
         <h2 className="hidden-sm">{text}</h2>
         <h3 className="hidden-lg">{text}</h3>
