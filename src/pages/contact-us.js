53a92f41fdf44afeda1a2234cbdb2e52bf3c332a
Sasha Klein <sashafklein@gmail.com>
2020-04-09 16:53:18 -0700

    Mobile contact styles

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -24,7 +24,7 @@ const ContactUs = () => {
       <div className="bg--off-white">
         <div className="container pb100">
           <div className="row flex-ai--fs flex-col--resp">
-            <div className="flex flex-col flex-ai--fs color--black col-5 flex-jc--fs">
+            <div className="flex flex-col flex-ai--fs color--black col-6 flex-jc--fs mb50">
               <h1 className="mt0 color--dark-purple">Leave us a note</h1>
               <p>
                 We're committed to building a tool that helps our world recover
@@ -35,12 +35,12 @@ const ContactUs = () => {
               </p>
               <p>Have questions or concerns? Want to help? Let us know!</p>
             </div>
-            <div className="col-2"></div>
+            <div className="col-1"></div>
             <form
               method="post"
               netlify-honeypot="bot-field"
               data-netlify="true"
-              className="flex flex-col col-5"
+              className="flex flex-col col-5 w100p--resp"
             >
               <input type="hidden" name="bot-field" />
               <label className="flex flex-col mv10">
@@ -49,7 +49,7 @@ const ContactUs = () => {
                   type="text"
                   name="name"
                   id="name"
-                  style={{ maxWidth: 500 }}
+                  className="w500 full-width--resp"
                 />
               </label>
               <label className="flex flex-col mv10">
@@ -59,7 +59,7 @@ const ContactUs = () => {
                   name="email"
                   id="email"
                   required
-                  style={{ maxWidth: 500 }}
+                  className="w500 full-width--resp"
                 />
               </label>
               <label className="flex flex-col mv10">
@@ -69,7 +69,7 @@ const ContactUs = () => {
                   name="subject"
                   id="subject"
                   required
-                  style={{ maxWidth: 500 }}
+                  className="w500 full-width--resp"
                 />
               </label>
               <label className="flex flex-col mv10">
@@ -79,18 +79,18 @@ const ContactUs = () => {
                   id="message"
                   required
                   rows="5"
-                  style={{ maxWidth: 500 }}
+                  className="w500 full-width--resp"
                 />
               </label>
               <div className="row mt20">
                 <button
-                  className="btn btn--royal-blue w150 mr20 mr0--resp"
+                  className="btn btn--royal-blue w150 w100p--resp mr20 mr0--resp mb20--resp"
                   type="submit"
                 >
                   Send
                 </button>
                 <input
-                  className="btn btn--dark-purple w150"
+                  className="btn btn--dark-purple w150 w100p--resp"
                   type="reset"
                   value="Clear"
                 />
